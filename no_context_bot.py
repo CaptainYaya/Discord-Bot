20551eefed43457384f4552c7b3d5c349a8459e3
CaptainYaya <144121523+CaptainYaya@users.noreply.github.com>
2025-03-29 07:17:21 +0100

    Add files via upload

---
+++ no_context_bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix= "/")
+
+@client.event
+async def on_ready():
+    print(f'{client.user} se connecte sans problème !')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("/test"):
+       return
+    
+    if message.content.startswith("Salut"):
+        await message.channel.send("Salut, comment vas-tu ?")
+    
+    if message.content.startswith("Tu vas bien ?"):
+        await message.channel.send("Je vais bien et toi ?")
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Salut à tous, je suis Alpha, le Fox Bot")
+
+
+
+client.run("Bot Token")
